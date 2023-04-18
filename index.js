b186c6839637f5fd9ca490f4709be969fd17da8b
Andrew Diamond <amdiamond107@gmail.com>
2023-04-17 22:46:09 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+// superbowlWin = (record) => {
+//     const result = record.find( record => record.result === "W" );
+//     return !!result ? result.year : undefined;
+//   }
+
+function superbowlWin(records) {
+    const result = records.find(record => record.result === "W");
+    if (result) {
+        return result.year
+    }
+    else {return undefined};
+};
